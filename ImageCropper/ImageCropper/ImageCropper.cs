961588340bc0d968d45d34e46af3c9f1eff5152d
kc8 <kyle@cooperkyle.com>
2020-12-16 15:07:49 -0500

    Added exception throwing rather then just crashing or invoking the failure method if not set

--- ImageCropper/ImageCropper/ImageCropper.cs
+++ ImageCropper/ImageCropper/ImageCropper.cs
@@ -30,6 +30,20 @@ namespace Stormlion.ImageCropper
 
         public string PageTitle { get; set; } = null;
 
+        /// <summary>
+        /// If passing in an existing image, this will be populated with its path
+        /// If not passing in an existing image, this will contain the original path of the image 
+        /// taken with Plugin.Crossmedia implementation
+        /// </summary>
+        public string OriginalFilePath { get; set; } = null;
+
+        /// <summary>
+        /// Removes the original image if you use Take Photo. 
+        /// Will not remove the original photo if using Gallery or pass in an image with imageFile
+        /// NOT YET IMPLEMENTED 
+        /// </summary>
+        public bool RemoveOriginal { get; set; } = false;
+
         public string SelectSourceTitle { get; set; } = "Select source";
 
         public string TakePhotoTitle { get; set; } = "Take Photo";
@@ -37,16 +51,19 @@ namespace Stormlion.ImageCropper
         public string PhotoLibraryTitle { get; set; } = "Pick from Photo Library";
 
         public string CancelButtonTitle { get; set; } = "Cancel";
+        
         public PhotoSize ImageSize { get; set; } = PhotoSize.Full;
+       
         public int ImageCompressionQuality { get; set; } = 100;
 
         public Action<string> Success { get; set; }
 
         public Action Failure { get; set;}
 
-        public async Task Show(Page page, string imageFile = null)
+        public async Task Show(Page page, string imageFile = null, bool RemoveOriginal = false)
         {
-            MediaFile file = null; //not doing this
+            MediaFile file = null; 
+            OriginalFilePath= imageFile; 
             if (imageFile == null)
             {
                 await CrossMedia.Current.Initialize(); 
@@ -57,6 +74,7 @@ namespace Stormlion.ImageCropper
                 {
                     if (!CrossMedia.Current.IsCameraAvailable || !CrossMedia.Current.IsTakePhotoSupported)
                     {
+                        if (Failure == null) throw new CameraIsNotAvailableException("The Camera is not available or photo functions are not supported"); 
                         Failure?.Invoke();
                         return;
                     }
@@ -71,18 +89,14 @@ namespace Stormlion.ImageCropper
                 {
                     if (!CrossMedia.Current.IsPickPhotoSupported)
                     {
+                        if (Failure == null) throw new PhotoPickingIsNotAvailable("Thee photo picker was not available"); 
                         Failure?.Invoke();
                         return;
                     }
-                    try
-                    {
-                        file = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions
-                        { SaveMetaData = true, CompressionQuality = 100 });
-                    }
-                    catch (Exception e)
-                    {
-                        System.Console.WriteLine();
-                    }
+
+                    //If permissions are not setup properly for CrossMedia it can cause crashing
+                    file = await CrossMedia.Current.PickPhotoAsync(new PickMediaOptions
+                    { SaveMetaData = true, CompressionQuality = 100 });
                 }
                 else
                 {
@@ -96,21 +110,27 @@ namespace Stormlion.ImageCropper
                     return;
                 }
 
+                OriginalFilePath = file.Path; 
                 imageFile = file.Path;
             }
 
             // small delay
             await Task.Delay(TimeSpan.FromMilliseconds(100));
             DependencyService.Get<IImageCropperWrapper>().ShowFromFile(this, imageFile); 
-
+        
             // dispose media file
-          //  if (file != null) file?.Dispose(); 
-            //file?.Dispose(); //Object Reference not set to an instance of an object 
+            file?.Dispose(); 
+            this.CleanUp();
         }
 
         public byte[] GetBytes(string imageFile)
         {
             return DependencyService.Get<IImageCropperWrapper>().GetBytes(imageFile);
+        }
+
+        private void CleanUp()
+        {
+            //TO BE IMPLEMENTED
         }
     }
 }
--- ImageCropper/ImageCropper/ImageCropperExceptions.cs
+++ ImageCropper/ImageCropper/ImageCropperExceptions.cs
@@ -6,8 +6,25 @@ namespace Stormlion.ImageCropper
 {
     //TODO Work on adding some exceptions for if somethiwng fails but some 
     // of this will depend on the Failure Action in ImageCropper.cs
-    class ImageCropperExceptions
+    
+    public class CameraIsNotAvailableException : Exception
     {
+        public CameraIsNotAvailableException() {} 
 
+        public CameraIsNotAvailableException(string message) : base(message) {}
+    }
+
+    public class PhotoPickingIsNotAvailable : Exception
+    {
+        public PhotoPickingIsNotAvailable() {} 
+
+        public PhotoPickingIsNotAvailable(string message) : base(message) {}
+    }
+
+    public class InputtedImageFileBranchingFailure: Exception
+    {
+        public InputtedImageFileBranchingFailure() {} 
+
+        public InputtedImageFileBranchingFailure(string message) : base(message) {}
     }
 }
